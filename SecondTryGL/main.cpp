6ea4119b4fb38068467b88c6b386db86b2d9ce85
kowalewskij <kowalewskij@student.mini.pw.edu.pl>
2019-01-05 14:43:53 +0100

    Added shader_s class

--- SecondTryGL/main.cpp
+++ SecondTryGL/main.cpp
@@ -1,9 +1,41 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <fstream>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
+//
+//std::string fileToString(const char *filename) {
+//	std::string s;
+//	size_t size;
+//	char*  str;
+//	std::fstream f(filename, (std::fstream::in | std::fstream::binary));
+//
+//	if (!f.is_open()) {
+//		return "";
+//	}
+//	else {
+//		size_t fileSize;
+//		f.seekg(0, std::fstream::end);
+//		size = fileSize = (size_t)f.tellg();
+//		f.seekg(0, std::fstream::beg);
+//
+//		str = new char[size + 1];
+//		if (!str) {
+//			f.close();
+//			return "";
+//		}
+//
+//		f.read(str, fileSize);
+//		f.close();
+//		str[size] = '\0';
+//
+//		s = str;
+//		delete[] str;
+//		return s;
+//	}
+//}
 
 const char *vertexShaderSource = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
@@ -39,15 +71,26 @@ int main() {
 	}
 
 	float vertices[] = {
-		-0.5f, -0.5f, 0.0f,
-		 0.5f, -0.5f, 0.0f,
-		 0.0f,  0.5f, 0.0f
+	 0.5f,  0.5f, 0.0f,
+	 0.5f, -0.5f, 0.0f,
+	-0.5f, -0.5f, 0.0f,
+	-0.5f,  0.5f, 0.0f
+	};
+	unsigned int indices[] = {
+		0, 1, 3,
+		1, 2, 3
 	};
 
-	unsigned int VBO, VAO;
+	unsigned int VBO, VAO, EBO;
+	glGenBuffers(1, &EBO);
 	glGenVertexArrays(1, &VAO);
-	glBindVertexArray(VAO);
 	glGenBuffers(1, &VBO);
+
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
@@ -99,8 +142,8 @@ int main() {
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		glUseProgram(shaderProgram);
-		glBindVertexArray(VAO); 
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glBindVertexArray(VAO);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
